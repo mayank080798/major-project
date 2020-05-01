8701050599247b9e40854007915f703427147269
Mayank <mayanklal08@icloud.com>
2020-05-01 21:11:12 +0530

    Add configuration

---
+++ config/keys.js
@@ -0,0 +1,19 @@
+module.exports={
+    mongodb:{
+        MONGODB_URL     :'mongodb+srv://mayank:mayank@cluster0-hwp5t.mongodb.net/major-project?retryWrites=true&w=majority',
+    },
+    googleAPI:{
+        CLIENT_ID       :'243977226705-3jsva9bjc6ob2edt5eb59jiv32ld2dr4.apps.googleusercontent.com',
+        CLIENT_SECRET   :'1V7cmNggNmk_zD6oBH8ROXkz',
+        CALLBACK_URL    :'/user/auth/google/callback',
+    },
+    session:{
+        COOKIE_SECRET   : 'mayank lal',
+        COOKIE_NAME     : 'sid',
+    },
+    sendgrid:{
+        SENDGRID_API_KEY:'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',
+
+    }
+    
+};
